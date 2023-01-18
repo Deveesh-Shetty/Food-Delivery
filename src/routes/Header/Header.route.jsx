fcc311ffc94300f2a5f3098aaa7af961d2da3637
rvitality <vital.renzjohn@gmail.com>
2023-01-19 00:02:44 +0800

    Add header component and its styles

---
+++ src/routes/Header/Header.route.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+import { HiOutlineShoppingBag } from "react-icons/hi";
+
+import "./Header.styles.scss";
+
+const Header = () => {
+    return (
+        <>
+            <header className="section-px">
+                <div className="left">
+                    <h1>
+                        <a href="#">kalim.</a>
+                    </h1>
+                </div>
+
+                <div className="right">
+                    <nav className="nav">
+                        <ul>
+                            <li>
+                                <a className="nav__link" href="#">
+                                    Menu
+                                </a>
+                            </li>
+                            <li>
+                                <a className="nav__link" href="#">
+                                    About
+                                </a>
+                            </li>
+                            <li>
+                                <a className="nav__link" href="#">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <a href="#" className="cart">
+                        <div className="cart__bag">
+                            <HiOutlineShoppingBag />
+                            <span className="cart__items">5</span>
+                        </div>
+
+                        <div>
+                            <div>Your Cart</div>
+                            <span className="cart__total">₱120.53</span>
+                        </div>
+                    </a>
+                </div>
+            </header>
+
+            <main>
+                <Outlet />
+            </main>
+        </>
+    );
+};
+
+export default Header;
