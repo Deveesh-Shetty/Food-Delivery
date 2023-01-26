3eabc656f301cb1f5bb7b7fa719955240a161385
rvitality <vital.renzjohn@gmail.com>
2023-01-26 22:21:03 +0800

    Limit length of products' preview description

--- src/components/ProductPreview/ProductPreview.component.jsx
+++ src/components/ProductPreview/ProductPreview.component.jsx
@@ -5,22 +5,32 @@ import { BsArrowRight } from "react-icons/bs";
 
 import "./ProductPreview.styles.scss";
 
-const ProductPreview = ({ product }) => {
-    const { productId, name, photo, prices, description } = product;
+const ProductPreview = ({ product, onProductSelect }) => {
+    let { productId, name, photo, prices, description } = product;
     const { size, cost } = prices;
 
     const productPath = name.toLowerCase().split(" ").join("_");
 
+    description =
+        description.split(" ").length > 8
+            ? description.split(" ").slice(0, 8).join(" ") + " ..."
+            : description;
+
     return (
-        <Link to={productPath} key={productId} className="product-preview">
+        <Link
+            onClick={onProductSelect.bind(null, product)}
+            to={productPath}
+            key={productId}
+            className="product-preview"
+        >
             <div className="photo">
                 <img src={photo} alt={name} />
             </div>
-            <div className="product">
-                <div className="product__price">₱ 15.99</div>
-                <h2 className="product__name">{name}</h2>
-                <p className="product__desc">{description}</p>
-                <div className="product__link">
+            <div className="details">
+                <div className="details__price">₱ 15.99</div>
+                <h2 className="details__name">{name}</h2>
+                <p className="details__desc">{description}</p>
+                <div className="details__link">
                     <span>Order Now</span>
                     <BsArrowRight />
                 </div>
