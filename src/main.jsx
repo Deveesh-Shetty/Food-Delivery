8b77e8b2912423997a35ca403b888c0dbd7ce059
rvitality <vital.renzjohn@gmail.com>
2023-01-27 22:42:01 +0800

    Wrap main app component with cart context provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { CartContextProvider } from "./context/CartContext";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <CartContextProvider>
+                <App />
+            </CartContextProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
